cb1c277db195dd1d8d25f0bdbd07bc5c2a108b2a
tildejustin <spacepepper3.14@gmail.com>
2023-06-12 18:39:09 -0400

    1.7 blockpos and blockstate changes

--- src/main/java/xyz/tildejustin/ctm_srigt_integration/mixin/WorldMixin.java
+++ src/main/java/xyz/tildejustin/ctm_srigt_integration/mixin/WorldMixin.java
@@ -2,7 +2,6 @@ package xyz.tildejustin.ctm_srigt_integration.mixin;
 
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,23 +14,25 @@ import xyz.tildejustin.ctm_srigt_integration.CTMTimer;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
-    @Shadow
-    public abstract BlockState getBlockState(BlockPos pos);
+
+    @Shadow public abstract Block getBlock(int x, int y, int z);
+
+    @Shadow public abstract int getBlockData(int x, int y, int z);
 
     @Inject(
-            method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z",
+            method = "setBlock",
             at = @At(
                     value = "HEAD"
             )
     )
-    private void ctm_srigt_integration$setBlockState(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> cir) {
+    private void ctm_srigt_integration$setBlockState(int x, int y, int z, Block block, int flags, int data, CallbackInfoReturnable<Boolean> cir) {
         InGameTimer timer = InGameTimer.getInstance();
         CTMCategory category = CTMTimer.categoryLinker.get(timer.getCategory());
         if (category == null || flags != 3) {
             return;
         }
         for (CTMCategory.Entry criterion : category.criteria) {
-            if (pos.equals(criterion.position)) {
+            if (new BlockPos(x, y, z).equals(criterion.position)) {
                 if (this.checkCriteria(category)) {
                     InGameTimer.complete();
                 }
@@ -44,9 +45,9 @@ public abstract class WorldMixin {
     // doesn't impact performance, just unnecessary
     private boolean checkCriteria(CTMCategory category) {
         for (CTMCategory.Entry entry : category.criteria.toArray(new CTMCategory.Entry[0])) {
-            boolean correctBlock = this.getBlockState(entry.position).getBlock().equals(Block.REGISTRY.get(entry.identifier));
+            boolean correctBlock = this.getBlock(entry.position.x, entry.position.y, entry.position.z).equals(Block.REGISTRY.get(entry.identifier));
             if (correctBlock) {
-                boolean correctData = this.getBlockState(entry.position).getBlock().getMeta(this.getBlockState(entry.position)) == entry.dataValue;
+                boolean correctData = this.getBlockData(entry.position.x, entry.position.y, entry.position.z) == entry.dataValue;
                 if (correctData) {
                     continue;
                 }
