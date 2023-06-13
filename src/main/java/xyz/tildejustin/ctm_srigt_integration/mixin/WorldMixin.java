f9061b682084ae3aa978ef32e94b7814fb9981bc
tildejustin <spacepepper3.14@gmail.com>
2023-06-12 21:06:18 -0400

    1.16-1.17

--- src/main/java/xyz/tildejustin/ctm_srigt_integration/mixin/WorldMixin.java
+++ src/main/java/xyz/tildejustin/ctm_srigt_integration/mixin/WorldMixin.java
@@ -6,11 +6,10 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.ChunkManager;
-import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,12 +17,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.tildejustin.ctm_srigt_integration.CTMCategory;
 import xyz.tildejustin.ctm_srigt_integration.CTMTimer;
 
-import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 @Mixin(ClientWorld.class)
 public abstract class WorldMixin extends World {
-    protected WorldMixin(LevelProperties levelProperties, DimensionType dimensionType, BiFunction<World, Dimension, ChunkManager> chunkManagerProvider, Profiler profiler, boolean isClient) {
-        super(levelProperties, dimensionType, chunkManagerProvider, profiler, isClient);
+
+    protected WorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DimensionType dimensionType, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed) {
+        super(properties, registryRef, dimensionType, profiler, isClient, debugWorld, seed);
     }
 
     @Inject(
@@ -39,9 +39,6 @@ public abstract class WorldMixin extends World {
             return;
         }
         for (CTMCategory.Entry criterion : category.criteria) {
-            System.out.println(blockPos);
-            System.out.println(criterion.position);
-            System.out.println();
             if (blockPos.equals(criterion.position)) {
                 if (this.checkCriteria(category)) {
                     InGameTimer.complete();
@@ -56,7 +53,6 @@ public abstract class WorldMixin extends World {
     private boolean checkCriteria(CTMCategory category) {
         for (CTMCategory.Entry entry : category.criteria.toArray(new CTMCategory.Entry[0])) {
             boolean correctBlock = this.getBlockState(entry.position).getBlock().equals(Registry.BLOCK.get(entry.identifier));
-            System.out.println(this.getBlockState(entry.position).getBlock());
             if (correctBlock) {
                 continue;
             }
