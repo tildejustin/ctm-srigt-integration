979c082ab0782ff4810cd479a1bcbb97052bbd35
tildejustin <spacepepper3.14@gmail.com>
2023-06-12 21:22:06 -0400

    1.19+

--- src/main/java/xyz/tildejustin/ctm_srigt_integration/mixin/WorldMixin.java
+++ src/main/java/xyz/tildejustin/ctm_srigt_integration/mixin/WorldMixin.java
@@ -3,11 +3,12 @@ package xyz.tildejustin.ctm_srigt_integration.mixin;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.profiler.Profiler;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
@@ -23,8 +24,8 @@ import java.util.function.Supplier;
 @Mixin(ClientWorld.class)
 public abstract class WorldMixin extends World {
 
-    protected WorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, RegistryEntry<DimensionType> registryEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed) {
-        super(properties, registryRef, registryEntry, profiler, isClient, debugWorld, seed);
+    protected WorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
     }
 
     @Inject(
@@ -53,7 +54,7 @@ public abstract class WorldMixin extends World {
     // doesn't impact performance, just unnecessary
     private boolean checkCriteria(CTMCategory category) {
         for (CTMCategory.Entry entry : category.criteria.toArray(new CTMCategory.Entry[0])) {
-            boolean correctBlock = this.getBlockState(entry.position).getBlock().equals(Registry.BLOCK.get(entry.identifier));
+            boolean correctBlock = this.getBlockState(entry.position).getBlock().equals(Registries.BLOCK.get(entry.identifier));
             if (correctBlock) {
                 continue;
             }
